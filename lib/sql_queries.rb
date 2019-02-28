03892e66513b9164617e20d359bad0392a2aa243
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-02-28 23:19:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,11 +30,10 @@ ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, projects.title, SUM(pledges.amount)
+"SELECT projects.category, SUM(pledges.amount)
 FROM projects
 JOIN pledges ON projects.id = pledges.project_id
-WHERE projects.category = 'music'
-GROUP BY projects.title"
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
