c950d944f0e08b0795d0486c582e10635646027e
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-02-28 23:10:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,11 @@ HAVING SUM(pledges.amount) >= funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount)
+FROM users
+JOIN pleges ON users.id = pleges.user_id
+GROUP BY users.name
+ORDER BY pledge.amount, users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
